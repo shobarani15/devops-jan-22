7448a832d5e2c19810e641b58c7b5cc1497950fb
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2022-04-21 10:45:15 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def print_ip(): 
-    hostname = socket.gethostname()
-    localip = socket.gethostbyname(hostname)
-    return localip
-    
+    return "This is homepage or root /"
+
+@app.route('/login')
+def print_ip(): 
+    return "This is login page or path /login"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)    
